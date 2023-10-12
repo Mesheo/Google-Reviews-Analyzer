418ae335f0d73217032a4a2c218cc1000e151fb0
Michel Ledig <m.ledig.mic@gmail.com>
2023-10-12 10:59:07 -0300

    FIRST connection with PostgreDB from lambda

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+const Sequelize = require('sequelize');
+
 module.exports.handler = async (event) => {
+	deuBom = '';
+	const sequelize = new Sequelize('postgres', 'michel', '4mazz0n!', {
+		host: 'business-analyzer.crrkmjqebzph.us-east-1.rds.amazonaws.com',
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true, 
+				rejectUnauthorized: false 
+			}
+		},
+		port: 5432,
+	});
+
+
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+		deuBom = "CONECTAMOS!!"
+
+		// Sincronize o modelo com o banco de dados
+		await sequelize.sync();
+		console.log('Modelo sincronizado com o banco de dados.');
+
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	} finally {
+		// Feche a conexão após a conclusão das operações
+		sequelize.close();
+	}
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "Meu deus esse lambda é meu realreal",
-				TITULO: siteTitle,
+				message: `Meu deus esse lambda é meu realreal ${deuBom}`,
 				input: event,
 			},
 			null,
