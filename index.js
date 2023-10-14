ee8dfaa794820dcbe3b2539146d468539f2a537a
Michel Ledig <m.ledig.mic@gmail.com>
2023-10-14 13:51:16 -0300

    modularizng db

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: 'config/.env'});
 const URL = "https://www.google.com.br/maps/place/Nema+Padaria+-+Niter%C3%B3i/@-22.9039012,-43.113625,17z/data=!3m1!4b1!4m6!3m5!1s0x998373996a193f:0xb7ce627a8de35c77!8m2!3d-22.9039012!4d-43.113625!16s%2Fg%2F11k3fns4xy?entry=ttu";
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
@@ -6,9 +6,13 @@ const crypto = require('crypto');
 const dbClient = require('./database/db');
 const Sequelize = require('sequelize')
 const reviewsCreationFunction = require('./models/reviews');
+const businessCreationFunction = require('./models/Businesses');
+
+const extractBusinessInfo = require('./src/scraper/extractBusinessInfo')
 
 dbClient.connect();
 const Reviews = reviewsCreationFunction(dbClient.sequelize, Sequelize);
+const Businesses = businessCreationFunction(dbClient.sequelize, Sequelize);
 
 function createReviewHash(reviewInfo) {
 	const reviewData = `${reviewInfo.author}${reviewInfo.stars}${reviewInfo.publishedAt}${reviewInfo.reviewText}${reviewInfo.hasPhoto}`;
@@ -27,13 +31,29 @@ async function extractReviewInfo(page) {
 	const pageContent = await page.content();
 	const $ = cheerio.load(pageContent);
 
-	$(reviewSelector).each(async (index, element) => {
+	const reviewDivs = $('.jftiEf.fontBodyMedium');
+	console.log("QUANTAs temos: \n", reviewDivs[0])
+	
+	// function extractReviewData(reviewDiv)
+	/*
+	$(reviewSelector).each((index, element) => {
 		const reviewcAard= $(element);
-		const author = reviewcAard.find('div.d4r55').text();
+		const author = reviewcAard.find('div.d4r55'd).text();
 		const stars = reviewcAard.find('span.kvMYJc img').length;
 		const reviewDate = reviewcAard.find('span.rsqaWe').text();
 		const reviewText = reviewcAard.find('span.wiI7pd').text();
 		const hasPhoto = reviewcAard.find('button.WEBjve img').length > 0;
+
+		console.log("Olha de onde vem o hasphoto: ", reviewcAard.find('button.WEBjve img').length)
+		[
+			{
+			hash1: {texto, nome, autor}
+			},
+			{
+			hash2: {texto2, nome2, autor2
+			}
+		]
+
 
 		const reviewInfo = {
 			author,
@@ -43,69 +63,35 @@ async function extractReviewInfo(page) {
 			hasPhoto,
 			
 		}
-
 		const reviewHash = createReviewHash(reviewInfo)
 
 		const review = await Reviews.create({...reviewInfo, reviewHash});
 		console.log("[WEBSCRAPER - ExtractReviewInfo] New Review added on database: ", review.dataValues)
 	});
+	*/
 }
 
-async function extractBusinessInfo(page) {
-	const topInfoDiv = await page.$eval('.TIHn2', (element) => element.innerHTML);
-	const $ = cheerio.load(topInfoDiv);
-
-	const name = $('h1.DUwDvf').text();
-	const ratingsAverage = parseFloat($('div.F7nice span[aria-hidden="true"]').text().replace(',', '.'));
-	const numberOfReviews = parseInt($("div.F7nice span[aria-label]").text().replace(/[()]/g, ''), 10);
-	const category = $('div.fontBodyMedium button.DkEaL').text();
-
-	const botInfoDiv = await page.$eval(`div[aria-label="Informações de ${name}"]`, (element) => element.innerHTML);
-	const $$ = cheerio.load(botInfoDiv);
-	const cardsbotInfoDiv = $$('button[aria-label*="Endereço:"], button[aria-label*="Telefone:"], a[aria-label*="Website:"]');
-
-	let address = "";
-	let telephone = "";
-	let website = "";
-	cardsbotInfoDiv.each(function () {
-		const label = $(this).attr('aria-label');
-		if (label.includes('Endereço:')) {
-			address = label.replace('Endereço: ', '');
-		} else if (label.includes('Telefone:')) {
-			telephone = label.replace('Telefone: ', '');
-		} else if (label.includes('Website:')) {
-			const href = $(this).attr('href');
-			website = href;
-		}
-	});
-
-	return {
-		name,
-		ratingsAverage,
-		numberOfReviews,
-		category,
-		address,
-		telephone,
-		website,
-	};
-}
 
 async function scraper(url) {
 	try {
 		const browser = await puppeteer.launch({ headless: false });
 		const page = await browser.newPage();
-		const avaliacoesSelector = '.RWPxGd button:nth-child(2)';
-
 		await page.goto(url);
-		await page.waitForSelector(avaliacoesSelector, { visible: true });
-		// const businessInfo = await extractBusinessInfo(page);
-		// console.log("[WEBSCRAPER] - BusinessInfo Data fetched:  ", businessInfo);
 
-		const reviewsSelector = '.MyEned .wiI7pd'
-		await page.click(avaliacoesSelector);
-		await page.waitForSelector(reviewsSelector, { visible: true });
-		await extractReviewInfo(page);
-		// await browser.close();
+		const avaliacoesSelector = '.RWPxGd button:nth-child(2)';
+		await page.waitForSelector(avaliacoesSelector, { visible: true });
+		const businessInfo = await extractBusinessInfo(page);
+		console.log("Adding BusinessesInfo into Database!..")
+		const business = await Businesses.create(businessInfo);
+
+
+		console.log("TEMOS id?? :", business.businessId)
+		
+		// await page.click(avaliacoesSelector);
+		// const reviewsSelector = '.MyEned .wiI7pd'
+		// await page.waitForSelector(reviewsSelector, { visible: true });
+		// await extractReviewInfo(page);
+		// // await browser.close();
 	} catch (e) {
 		console.error("Error occurred: ", e);
 	}
---
+++ src/scraper/extractBusinessInfo.js
@@ -0,0 +1,48 @@
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const dbClient = require('../../database/db');
+const businessCreationFunction = require('../../models/businesses');
+
+
+
+
+module.exports = async function extractBusinessInfo(page) {
+    const topInfoDiv = await page.$eval('.TIHn2', (element) => element.innerHTML);
+    const $ = cheerio.load(topInfoDiv);
+
+    const name = $('h1.DUwDvf').text();
+    const ratingsAverage = parseFloat($('div.F7nice span[aria-hidden="true"]').text().replace(',', '.'));
+    const numberOfReviews = parseInt($("div.F7nice span[aria-label]").text().replace(/[()]/g, ''), 10);
+    const category = $('div.fontBodyMedium button.DkEaL').text();
+
+    const botInfoDiv = await page.$eval(`div[aria-label="Informações de ${name}"]`, (element) => element.innerHTML);
+    const $$ = cheerio.load(botInfoDiv);
+    const cardsbotInfoDiv = $$('button[aria-label*="Endereço:"], button[aria-label*="Telefone:"], a[aria-label*="Website:"]');
+
+    let address = "";
+    let telephone = "";
+    let website = "";
+    cardsbotInfoDiv.each(function () {
+        const label = $(this).attr('aria-label');
+        if (label.includes('Endereço:')) {
+            address = label.replace('Endereço: ', '');
+        } else if (label.includes('Telefone:')) {
+            telephone = label.replace('Telefone: ', '');
+        } else if (label.includes('Website:')) {
+            const href = $(this).attr('href');
+            website = href;
+        }
+    });
+
+    const businessInfo = {
+        name,
+        address,
+        telephone,
+        website,
+        category,
+        ratingsAverage,
+        numberOfReviews,
+    }
+    console.log("[WEBSCRAPER - extractbusinessInfo] Data Succesfully fetched: ", businessInfo);
+    return businessInfo
+}
