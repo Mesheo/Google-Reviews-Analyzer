66753fecb3d94f23a2c6ca888f30cf0a3e22f184
Michel Ledig <m.ledig.mic@gmail.com>
2023-10-15 19:41:03 -0300

    removing unecessary libraries

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config({ path: 'config/.env' });
-const crawl = require('./src/functions/crawl');
+const runScraper = require('./src/functions/runScraper');
 const URL = "https://www.google.com.br/maps/place/Nema+Padaria+-+Niter%C3%B3i/@-22.9039012,-43.113625,17z/data=!3m1!4b1!4m6!3m5!1s0x998373996a193f:0xb7ce627a8de35c77!8m2!3d-22.9039012!4d-43.113625!16s%2Fg%2F11k3fns4xy?entry=ttu";
 
 module.exports.handler = async (event) => {
-	await crawl(URL);
+	await runScraper(URL);
 	return {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
 				status: 'pending',
-				message: `URL received. Scrapper has successfully collected the data and saved on database`,
+				message: `URL received. Scrapper has successfully started to run.`,
 				input: event,
 			},
 		),
--- src/functions/runScraper.js
+++ src/functions/runScraper.js
@@ -1,18 +1,26 @@
-const puppeteer = require('puppeteer');
 const businessScraper = require('../scrapers/businessScraper')
 const reviewScraper = require('../scrapers/reviewScraper')
 const dbClient = require('../database/db');
+const chromium = require('chrome-aws-lambda');
 
-module.exports = async function crawler(url) {
+module.exports = async function runScraper(url) {
 	try {
 		dbClient.connect();
 
-		const browser = await puppeteer.launch({ headless: false });
+		const browser = await chromium.puppeteer
+        .launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless
+        });
 		const page = await browser.newPage();
 		await page.goto(url);
 
 		const { businessId } = await businessScraper(page, dbClient.sequelize);
 		await reviewScraper(page, dbClient.sequelize, businessId);
+
+		await browser.close();
 	} catch (e) {
 		console.error("Error occurred: ", e);
 	}
